8f6c07effc0da3e217b656f87babdef1132b9c7a
15708498948 <850050514@qq.com>
2019-09-26 15:47:47 +0800

    Site updated: 2019-09-26 15:47:46

---
+++ dist/music.js
@@ -0,0 +1,9 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    audio: [{
+        name: 'name',
+        artist: 'artist',
+        url: 'http://www.ytmp3.cn/down/73421.mp3',
+        cover: 'cover.jpg'
+    }]
+});
